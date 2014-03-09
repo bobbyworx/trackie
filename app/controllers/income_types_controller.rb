f7a6b4dab363db2e173be2d05bc6c7b0eadeda6d
Bogdan Popa <bogdan.popa.a@gmail.com>
2014-03-09 12:20:05 +0000

    #1: edit/delete income_type operations/ dropdown menu(not yet functional)

--- app/controllers/income_types_controller.rb
+++ app/controllers/income_types_controller.rb
@@ -5,7 +5,7 @@ class IncomeTypesController < ApplicationController
   end
 
   def create
-    @income_type = IncomeType.new(params[:income_type])
+    @income_type = IncomeType.new(income_type_params)
     if @income_type.save
       redirect_to action: 'index', notice: "You have successfully created an income type!"
     else
@@ -26,7 +26,7 @@ class IncomeTypesController < ApplicationController
 
   def update
     income_type
-     if @income_type.update(params[:income_type])
+     if @income_type.update(params[:income_type].permit(:name))
       redirect_to @income_type
     else
       render 'edit'
@@ -40,9 +40,18 @@ class IncomeTypesController < ApplicationController
 
   end
 
+  def income_type_params
+    params.require(:income_type).permit(:name)
+  end
+
   def income_type
      @income_type ||= IncomeType.find(params[:id])
   end
+
+  #def parents
+    #income_type
+    #@parents = income_type.parent_income_type
+  #end
 
   #def to_s
     #income_type.parent_income_type
