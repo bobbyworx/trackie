1cdc2d5abcc650b1e6422aa23b05b13019069f4b
Bogdan Popa <bogdan.popa.a@gmail.com>
2014-03-10 16:59:03 +0000

    #1: installed bootstrap theme/added validation for name uniqueness/not yet functional dropdown parent condition

--- app/models/income_type.rb
+++ app/models/income_type.rb
@@ -1,5 +1,5 @@
 class IncomeType < ActiveRecord::Base
-  validates :name, presence: true
+  validates :name, presence: true, :uniqueness => true
   has_many :child_income_types, :class_name => "IncomeType",
                   :foreign_key => "parent_income_type_id"
   belongs_to :parent_income_type, :class_name => "IncomeType", 
