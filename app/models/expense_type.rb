88dced3d40cbc2b0ff99b522d1d04cdbf1d758ac
Bogdan Popa <bogdan.popa.a@gmail.com>
2014-04-23 14:16:05 +0000

    fixed expense_type validation loop bug/ added devise invitable

--- app/models/expense_type.rb
+++ app/models/expense_type.rb
@@ -8,14 +8,13 @@ class ExpenseType < ActiveRecord::Base
   belongs_to :parent_expense_type, :class_name => "ExpenseType"
 
   def name_different_from_parent
-    if self.id == self.parent_expense_type_id
+    if self.name == self.parent_expense_type.try(:name)
       errors.add(:expense_type, "name has to be different from parent's name!")
     end
   end
 
   def extended
-  self.child_expense_types.to_a << self
-  #[self] << self.child_income_types.to_a
+    self.child_expense_types.to_a << self
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :confirmable, :registerable,
+  devise :invitable, :database_authenticatable, :confirmable, :registerable,
          :recoverable, :rememberable, :trackable, :secure_validatable, :confirmable
 
   validates :username, length: { minimum: 4, :message => "The username should have more than 4 characters"}
